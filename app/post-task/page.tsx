9dff67d56b717951524406b5fa45a30d2fd114e1
mihailgybkin-dot <mihailgybkin@gmail.com>
2025-11-01 21:13:15 +0300

    Update page.tsx

--- app/post-task/page.tsx
+++ app/post-task/page.tsx
@@ -1,10 +1,23 @@
-'use client';
-import AddressPicker from '@/components/AddressPicker';
+"use client";
 
-// ...
-<AddressPicker
-  onChange={(v) => {
-    // сохраним координаты и адрес в состояние формы
-    setForm((p) => ({ ...p, address: v.address, lat: v.lat, lng: v.lng }));
-  }}
-/>
+import Header from "../../components/Header";
+import YandexAddressPicker from "../../components/YandexAddressPicker";
+
+export default function PostTaskPage() {
+  return (
+    <>
+      <Header />
+      <div className="container section">
+        <h1 className="h2">Разместить задание</h1>
+        <div className="card">
+          <form className="form">
+            <input className="input" placeholder="Заголовок задания" />
+            <input className="input" placeholder="Бюджет (₽)" type="number" />
+            <YandexAddressPicker />
+            <button className="btn btn--primary" type="button">Создать задание</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
